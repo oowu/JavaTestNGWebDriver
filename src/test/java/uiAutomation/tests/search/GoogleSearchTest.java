fd408620f33bac9e7eaec0421b135a89b62aa333
Bohdan <pbohdanpl@gmail.com>
2018-10-13 14:51:37 -0700

    changes expectedResult

--- src/test/java/uiAutomation/tests/search/GoogleSearchTest.java
+++ src/test/java/uiAutomation/tests/search/GoogleSearchTest.java
@@ -23,7 +23,7 @@ public class GoogleSearchTest extends BaseTest {
         resultPage.printOutAmountOfResult();
 
         String actualPageTitle = resultPage.getPageTitle();
-        String expectedPageTitle = "query";
+        String expectedPageTitle = "query - Google Search";
 
         Assert.assertEquals(actualPageTitle, expectedPageTitle);
     }
